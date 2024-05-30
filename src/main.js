020a39e0a9baabb1945abc1bc30fb439202586d1
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-05-31 00:34:21 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/about.js';
+import './js/skills.js';
+import './js/projects.js';
+import './js/benefits.js';
+import './js/faq.js';
+import './js/reviews.js';
+import './js/work-together.js';
+import './js/footer.js';
