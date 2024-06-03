23dd84f4c8554436899e252060929f9db21fd2d2
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-06-03 03:42:58 +0100

    added_js_scroll

--- src/js/header.js
+++ src/js/header.js
@@ -17,3 +17,19 @@ headerMenu.forEach(item => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('click', function(event) {
+        if (event.target.matches('a[href^="#"]')) {
+            event.preventDefault();
+            const id = event.target.getAttribute('href');
+            const targetElement = document.querySelector(id);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        }
+    });
+});
