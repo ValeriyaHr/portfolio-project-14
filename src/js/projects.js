674549032d9723bfc485aa6be3cbd2e028251f35
biletskakatia <152877295+biletskakatia@users.noreply.github.com>
2024-06-03 17:49:31 +0300

    new update img

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,56 +3,56 @@ document.addEventListener("DOMContentLoaded", () => {
     const loadMoreButton = document.querySelector('.load-more-button');
     const projects = [
         {
-            imgSrc1x: '.src/img/energy-1x.jpg',
-            imgSrc2x: '.src/img/energy-2x.jpg',
+            imgSrc1x: './img/energy-1x.jpg',
+            imgSrc2x: './img/energy-2x.jpg',
             alt: 'energy projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'ENERGY FLOW WEBSERVICE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
         {
-            imgSrc1x: '.src/img/fruitbox-1x.jpg',
-            imgSrc2x: '.src/img/fruitbox-2x.jpg',
+            imgSrc1x: './img/fruitbox-1x.jpg',
+            imgSrc2x: './img/fruitbox-2x.jpg',
             alt: 'fruitbox projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'FRUITBOX ONLINE STORE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
         {
-            imgSrc1x: '.src/img/jewelry-1x.jpg',
-            imgSrc2x: '.src/img/jewelry-2x.jpg',
+            imgSrc1x: './img/jewelry-1x.jpg',
+            imgSrc2x: './img/jewelry-2x.jpg',
             alt: 'jewelry projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'CHEGO JEWELRY WEBSITE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
             {
-            imgSrc1x: '.src/img/mimino-1x.jpg',
-            imgSrc2x: '.src/img/mimino-2x.jpg',
+            imgSrc1x: './img/mimino-1x.jpg',
+            imgSrc2x: './img/mimino-2x.jpg',
             alt: 'mimino projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'MIMINO WEBSITE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
         {
-            imgSrc1x: '.src/img/power-1x.jpg',
-            imgSrc2x:'.src/img/power-2x.jpg',
+            imgSrc1x: './img/power-1x.jpg',
+            imgSrc2x:'./img/power-2x.jpg',
             alt: 'power projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'POWER PULSE WEBSERVICE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
         {
-            imgSrc1x: '.src/img/starlight-1x.jpg',
-            imgSrc2x: '.src/img/starlight-2x.jpg',
+            imgSrc1x: './img/starlight-1x.jpg',
+            imgSrc2x: './img/starlight-2x.jpg',
             alt: 'starlight projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'STARLIGHT STUDIO LANDING PAGE',
             link: 'https://github.com/Kate-Iva/portfolio-project-14'
         },
 {
-            imgSrc1x: '.src/img/vyshyvanka-1x.jpg',
-            imgSrc2x: '.src/img/vyshyvanka-2x.jpg',
+            imgSrc1x: './img/vyshyvanka-1x.jpg',
+            imgSrc2x: './img/vyshyvanka-2x.jpg',
             alt: 'vyshyvanka projects',
             stack: 'React, JavaScript, Node JS, Git',
             title: 'VYSHYVANKA VIBES LANDING PAGE',
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <div class="projects-item-title">${project.title}</div>
                     <a href="${project.link}" class="projects-btn" target="_blank">VISIT
                         <svg class="projects-icon" width="24" height="24">
-                            <use href=".src/img/icons.svg#icon-arrow-visit"></use>
+                            <use href="./img/icons.svg#icon-arrow-visit"></use>
                         </svg>
                     </a>
                 </div>
